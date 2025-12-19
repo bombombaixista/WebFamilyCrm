647a247d46a694f316c524f5dce1a8688df2f7bc
bombombaixista <seuemail@exemplo.com>
2025-12-18 21:30:05 -0300

    Atualização: descrição da modificação feita

--- Program.cs
+++ Program.cs
@@ -1,42 +1,14 @@
-﻿using Kanban.Data;
-using Kanban.Services;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.Cookies;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<SalesPipelineContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(
-            builder.Configuration.GetConnectionString("DefaultConnection")
-        )
-    )
-);
-
-// 👇 ISSO AQUI É A CHAVE
-builder.Services.AddHttpClient<ApiService>();
-
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.LoginPath = "/Login/Index";
-        options.LogoutPath = "/Login/Logout";
-    });
+﻿var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
